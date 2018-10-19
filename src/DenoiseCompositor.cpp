024c7f97a631abdfa1f0e46457a4c6b35ed1cba5
Phil Guo <phil.guo@pocketgems.com>
2018-10-18 17:31:05 -0700

    Setup offline compiling compute shader with Visual Studio

--- src/DenoiseCompositor.cpp
+++ src/DenoiseCompositor.cpp
@@ -1,8 +1,11 @@
 #include "stdafx.h"
 #include "DenoiseCompositor.h"
+#include "CompiledShaders/Compute.hlsl.h"
 #include "nv_helpers_dx12/RootSignatureGenerator.h"
 #include "DXSampleHelper.h"
 #include <D3Dcompiler.h>
+
+// #define RUNTIME_COMPILE_SHADER
 
 using nv_helpers_dx12::RootSignatureGenerator;
 
@@ -11,19 +14,23 @@ DenoiseCompositor::DenoiseCompositor(DXRFramework::RtContext::SharedPtr context)
 {
     auto device = context->getDevice();
 
-    ComPtr<ID3DBlob> computeShader;
-    ComPtr<ID3DBlob> error;
-
-    UINT compileFlags = D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION;
-    ThrowIfFailed(D3DCompileFromFile(L"..\\assets\\shaders\\Compute.hlsl", nullptr, nullptr, "main", "cs_5_0", compileFlags, 0, &computeShader, &error));
-
     RootSignatureGenerator rsConfig;
     rsConfig.AddHeapRangesParameter({ {0 /* u0 */, 1, 0, D3D12_DESCRIPTOR_RANGE_TYPE_UAV, 0} });
     mComputeRootSignature = rsConfig.Generate(device, false);
 
     D3D12_COMPUTE_PIPELINE_STATE_DESC computePsoDesc = {};
     computePsoDesc.pRootSignature = mComputeRootSignature.Get();
+
+#if defined(RUNTIME_COMPILE_SHADER)
+    LPCWSTR filePath = L"..\\assets\\shaders\\Compute.hlsl";
+    UINT compileFlags = D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION;
+    ComPtr<ID3DBlob> computeShader;
+    ComPtr<ID3DBlob> error;
+    ThrowIfFailed(D3DCompileFromFile(filePath, nullptr, nullptr, "main", "cs_5_0", compileFlags, 0, &computeShader, &error));
     computePsoDesc.CS = CD3DX12_SHADER_BYTECODE(computeShader.Get());
+#else
+    computePsoDesc.CS = CD3DX12_SHADER_BYTECODE(g_pCompute, ARRAYSIZE(g_pCompute));
+#endif
 
     ThrowIfFailed(device->CreateComputePipelineState(&computePsoDesc, IID_PPV_ARGS(&mComputeState)));
     NAME_D3D12_OBJECT(mComputeState);
